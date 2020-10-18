3e899cad84fbf8395b10c42586cda8efcfca620f
Jeremy Ruby <JeremyeRuby@gmail.com>
2020-10-18 17:55:17 -0400

    Updated Assignment file

--- assignment1.py
+++ assignment1.py
@@ -4,29 +4,30 @@ conn = sqlite3.connect('assignment.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_data1( \
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_data2( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_fileType TEXT \
         )")
 
 col_fileType = ('information.docx','Hello.txt','myImage.png','myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
 
-for x in col_fileType:
-    x.endswith(".txt")
-    
-conn = sqlite3.connect('assignment.db')
-
-
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_data1(col_fileType) VALUES(?),[x]")
-
-
+    for x in col_fileType:
+        if x.endswith(".txt"):
+            cur.execute("INSERT INTO tbl_data2(col_fileType) VALUES(?)",(x,))
     conn.commit()
 conn.close()
 
+conn = sqlite3.connect('assignment.db')
 
-print(cur.rowcount, "record inserted.")
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tbl_data2")
+    results = cur.fetchall()
+    print(results)
+conn.close()
+
 
 
 
