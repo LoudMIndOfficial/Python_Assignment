13b15054d584d9ace70e6f56842210666a3bd08b
Jeremy Ruby <JeremyeRuby@gmail.com>
2020-10-21 18:57:45 -0400

    assignment redone

--- inheritanceexample.py
+++ inheritanceexample.py
@@ -1,13 +1,19 @@
-class User:
-    name = 'No Name Provided'
-    email = ' '
-    password = '1234abcd'
-    account_number = 0
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-class Employee(User):
-    base_pay = 11.00
-    department = 'General'
+    def get_description_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
 
-class Customer(User):
-    mailing_address = ' '
-    mailing_list = True
+class electricCar(Car):
+    def __init__(self,make,model,year):
+        #initialize attributes of the parent class
+        super().__init__(make,model,year)
+
+my_hummer = electricCar('GMC','Hummer',2022)
+print(my_hummer.get_description_name())
+
