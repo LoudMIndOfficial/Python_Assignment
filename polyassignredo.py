979e231b802431020e9020cbffdeac7ea3a3d86a
Jeremy Ruby <JeremyeRuby@gmail.com>
2020-10-25 20:36:58 -0400

    Re submission 3 of poly assignment

--- polyassignredo.py
+++ polyassignredo.py
@@ -14,19 +14,24 @@ class mammal:
 
 class Bear(mammal):
     def __init__(self,first_name,last_name="Bear",
-                 skeleton="Bone", fur=True):
+                 skeleton="Bone", fur=True,stomach_state=True,teeth="Sharp"):
         self.first_name = first_name
         self.last_name = last_name
         self.skeleton = skeleton
         self.fur = fur
+        self.stomach_state = stomach_state
+        self.teeth = teeth
 
 class dolphin(mammal):
     def __init__(self,first_name,last_name="Dolphin",
-                 skeleton="Bone", fur=False):
+                 skeleton="Bone", fur=False,skin="Rubbery and Smooth",breath_air=True):
         self.first_name = first_name
         self.last_name = last_name
         self.skeleton = skeleton
         self.fur = fur
+        self.skin= skin
+        self.breath_air = breath_air
+        
 
     def walk(self):
         print("Dolphins can not walk they swim everywhere.")
@@ -40,6 +45,8 @@ print(barry.skeleton)
 print(barry.fur)
 barry.walk()
 barry.running()
+print(barry.stomach_state)
+print(barry.teeth)
 
 donny = dolphin("Donny the")
 print(donny.first_name + " " + donny.last_name)
@@ -47,6 +54,8 @@ donny.walk()
 donny.running()
 print(donny.skeleton)
 print(donny.fur)
+print(donny.skin)
+print(donny.breath_air)
 
 
 
