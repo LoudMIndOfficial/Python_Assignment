8d9528446ade8b352ff9644fe6a207e4d77e829a
Jeremy Ruby <JeremyeRuby@gmail.com>
2020-11-05 12:27:36 -0500

    Updated File

--- File Transfer.py
+++ File Transfer.py
@@ -4,7 +4,36 @@ import tkinter
 from tkinter import *
 import shutil
 import os
-import time
+from datetime import datetime
+import sqlite3
+
+conn = sqlite3.connect('BrowseDB.db')   #creates a database by changing the within the database will be saved in the location of the ".py" file. 
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtbrowse( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fileType TEXT \
+        )")
+
+col_fileType = ('CopyrightNotice.txt','New Text Document 1.txt','ThirdPartyNotices.txt')
+
+with conn:
+    cur = conn.cursor()
+    for x in col_fileType:
+        if x.endswith(".txt"):
+            cur.execute("INSERT INTO tbl_txtbrowse(col_fileType) VALUES(?)",(x,))
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('BrowseDB.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tbl_txtbrowse")
+    results = cur.fetchall()
+    print(results)
+conn.close()
 
 class ParentWindow(Frame):
     def __init__ (self, master):
@@ -42,17 +71,21 @@ class ParentWindow(Frame):
         self.btnCancel.grid(row=2,column=2, padx=(30,90), pady=(30,0), sticky=NE)
 
     def src_directory(self): #we don't want to name it askdirectory, as that exists in tkinter
-       src= tkFileDialog.askdirectory(self.txtBrowse) #this creates a variable to which the user's selection will be stored
+       src = tkFileDialog.askdirectory(self.txtBrowse) #this creates a variable to which the user's selection will be stored
        self.txtBrowse.Insert(0, src)
 
-    def src_directory(self): #we don't want to name it askdirectory, as that exists in tkinter
-       src= tkFileDialog.askdirectory(self.txtBrowse1) #this creates a variable to which the user's selection will be stored
+    def src_directory1(self): #we don't want to name it askdirectory, as that exists in tkinter
+       src = tkFileDialog.askdirectory(self.txtBrowse1) #this creates a variable to which the user's selection will be stored
        self.txtBrowse1.Insert(0, src)
                                                       
     def submit(self):
        path = self.txtBrowse.get()
        path2 = self.txtBrowse1.get()
        dir_list = os.listdir(path)
+       mod_time = os.stat('*.txt').st_mtime
+       print(datetime.fromtimestamp(mod_time))
+       
+       
        print(os.path.join(path, "*.txt"))
        print("Files and directories in '", path, "' :")
 
